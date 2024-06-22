f15eaafadd493db68c13225f926d1e2d6060692d
shubhamgonjari <gonjari31@gmail.com>
2024-06-22 13:24:22 +0530

    Requirements added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sign-language
+
+
+# How to run 
+
+```bash
+conda create -n signlang python=3.7 -y
+```
+```bash
+conda activate signlang
+```
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Shubham',
+    author_email= 'gonjari31@hmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
